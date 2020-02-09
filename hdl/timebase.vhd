4ea73c1b404feb9edd7a0eb86ec80dc979165c83
Lemmer El Assal <lelassal@hushmail.com>
2020-02-09 12:09:32 +0100

    Updated timebase.vhd.

--- hdl/timebase.vhd
+++ hdl/timebase.vhd
@@ -19,7 +19,15 @@ end timebase;
 
 architecture Behavioural of timebase is
 
-  signal time_ns, n_time_ns, time_s, n_time_s : std_logic_vector(31 downto 0);
+  signal counter, n_counter, 
+  time_ns, n_time_ns, time_s, n_time_s : std_logic_vector(31 downto 0);
+
+  constant ADDR_TIMEBASE_NANOS : std_logic_vector(31 downto 0) := base_address + X"00000000";
+  constant ADDR_TIMEBASE_SEC : std_logic_vector(31 downto 0) := base_address + X"00000001";
+  constant ADDR_TIMEBASE_DELAY_NS : std_logic_vector(31 downto 0) := base_address + X"00000002";
+  constant ADDR_TIMEBASE_DELAY_S : std_logic_vector(31 downto 0) := base_address + X"00000003";
+
+  signal state, n_state : integer range 0 to 1 := 0;
 
 begin
 
@@ -28,48 +36,74 @@ begin
   if rst = '1' then
     time_ns <= (others => '0');
     time_s <= (others => '0');
+    counter <= (others => '0');
+    state <= 0;
   elsif rising_edge(clk) then
-    if (mem_addr = base_address) and (mem_we = '1') then
-      time_ns <= mem_wdata;
-    else 
-      time_ns <= n_time_ns;
-    end if;
-
-
-    if (mem_addr = (base_address+X"00000004")) and (mem_we = '1') then
-      time_s <= mem_wdata;
-    else 
-      time_s <= n_time_s;
-    end if;
-
+    time_ns <= n_time_ns;
+    time_s <= n_time_s;
+    counter <= n_counter;
+    state <= n_state;
   end if;
 end process;
 
-process(time_ns, time_s)
-begin
-  n_time_ns <= time_ns + X"00000001";
-  n_time_s <= time_s;
-  if time_ns >= X"05F5E100" then
-    n_time_ns <= (others => '0');
-    n_time_s <= time_s + X"00000001";
-  end if;
-end process;
 
-process(mem_addr, time_s, time_ns)
+process(state, time_ns, time_s, mem_addr, mem_we, mem_wdata, counter)
 begin
+  n_state <= state;
+  mem_rdata <= (others => '0');
   mem_rdy <= '1';
   mem_wack <= '1';
   address_valid <= '1';
-  case mem_addr is
-    when base_address =>
-      mem_rdata <= time_ns;
-    when base_address + X"00000004" =>
-      mem_rdata <= time_s;
+
+  n_time_ns <= time_ns + X"0000000A";
+  n_time_s <= time_s;
+  if time_ns >= X"3B9ACA00" then
+    n_time_ns <= (others => '0');
+    n_time_s <= time_s + X"00000001";
+  end if;
+
+  case state is
+    when 0 =>
+      case mem_addr is
+        when ADDR_TIMEBASE_NANOS =>
+          mem_rdata <= time_ns;
+          if mem_we = '1' then
+            n_time_ns <= mem_wdata;
+          end if;
+
+        when ADDR_TIMEBASE_SEC =>
+          mem_rdata <= time_s;
+          if mem_we = '1' then
+            n_time_s <= mem_wdata;
+          end if;
+
+        when ADDR_TIMEBASE_DELAY_NS =>
+          n_counter <= mem_wdata;
+          mem_wack <= '0';
+          if mem_we = '1' then
+            n_state <= 1;
+          end if;
+          
+        when others =>
+          address_valid <= '0';
+          mem_rdy <= 'Z';
+          mem_wack <= 'Z';
+          mem_rdata <= (others => 'Z');
+      end case;
+
+    when 1 =>
+      mem_wack <= '0';
+      if counter < X"0000000A" then
+        mem_wack <= '1';
+        if mem_we = '0' then
+          n_state <= 0;
+        end if;
+      else
+        n_counter <= counter - X"0000000A";
+      end if;
+
     when others =>
-      address_valid <= '0';
-      mem_rdy <= 'Z';
-      mem_wack <= 'Z';
-      mem_rdata <= (others => 'Z');
+      n_state <= 0;
   end case;
 end process;
 
